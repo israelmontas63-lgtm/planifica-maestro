95f4d006741ef32adb49185523dbc5c0eab88963
Israel <Israelmonta63@gmail.com>
2026-09-17 15:14:37 -0400

    fix(vite): configurar base relativa para despliegue universal y soporte GitHub Pages

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
 export default defineConfig({
+  base: "./",
   resolve: {
     alias: {
       docx: path.resolve(__dirname, "../server/node_modules/docx"),
